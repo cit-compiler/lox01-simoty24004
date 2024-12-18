3e2e0373a2637312b12e6d4d026910f8da8e51fc
simoty24004 <ciyu24004@g.nihon-u.ac.jp>
2024-12-18 14:49:31 +0900

    modify Parser.java

--- codes/Parser.java
+++ codes/Parser.java
@@ -5,6 +5,9 @@ import java.util.List;
 import static com.craftinginterpreters.lox.TokenType.*;
 
 class Parser {
+
+  private static class ParseError extends RuntimeException {}
+
   private final List<Token> tokens;
   private int current = 0;
 
@@ -39,6 +42,14 @@ class Parser {
     return false;
   }
 
+  private Token consume(TokenType type, String message) {
+    if (check(type)) return advance();
+
+    throw error(peek(), message);
+  }
+
+
+
   private boolean check(TokenType type) {
     if (isAtEnd()) return false;
     return peek().type == type;
@@ -60,6 +71,13 @@ class Parser {
   private Token previous() {
     return tokens.get(current - 1);
   }
+
+  private ParseError error(Token token, String message) {
+    Lox.error(token, message);
+    return new ParseError();
+  }
+
+
 
   private Expr comparison() {
     Expr expr = term();
